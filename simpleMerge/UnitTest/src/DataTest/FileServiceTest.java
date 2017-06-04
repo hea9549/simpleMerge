d3e0603cec2583332c3f8f494cd5912a13d81000
smjkm4050 <smjkm4050@naver.com>
2017-06-04 23:16:34 +0900

    data test 123123

---
+++ simpleMerge/UnitTest/src/DataTest/FileServiceTest.java
@@ -0,0 +1,50 @@
+package DataTest;
+
+import Data.ComparableBlock;
+import Data.ComparableString;
+import Data.FileService;
+import junit.framework.TestCase;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import static org.easymock.EasyMock.*;
+
+/**
+ * Created by jgm on 6/4/17.
+ */
+public class FileServiceTest extends TestCase {
+    FileService mock;
+    RunFileService runner;
+    String filePath = new String();
+    ArrayList<ComparableString> result = new ArrayList<>();
+    File fileToSave;
+    ArrayList<String> contentsToSave = new ArrayList<>();
+
+    protected void setUp() throws Exception {
+        mock = createMock(FileService.class);
+        runner = new RunFileService();
+        runner.setFs(mock);
+        super.setUp();
+    }
+
+    public void testGetContents() {
+        expect(mock.getContents(filePath)).andReturn(result);
+        replay(mock);
+        assertEquals(result, runner.getContents(filePath));
+        verify(mock);
+    }
+    public void testGetFilePath(){
+        expect(mock.getFilePath()).andReturn(filePath);
+        replay(mock);
+        assertEquals(filePath, runner.getFilePath());
+        verify(mock);
+    }
+    public void testSaveFile(){
+        mock.saveFile(fileToSave, contentsToSave);
+        replay(mock);
+        runner.saveFile(fileToSave, contentsToSave);
+        verify(mock);
+    }
+
+}
